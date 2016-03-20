3c825bf22e3f7733ad4c5d566a561fcd53d9f066
eliothedeman <eliot.d.hedeman@gmail.com>
2016-03-20 01:34:59 -0400

    Added race functions.

--- examples/select.go
+++ examples/select.go
@@ -6,50 +6,43 @@ import (
 	"time"
 )
 
-func race(name string, smart bool) chan int {
-	out := make(chan int)
-	var t <-chan time.Time
-	if smart {
-		t = time.Tick(time.Second / 2)
-	} else {
-		t = time.Tick(time.Duration(int(rand.Float64() * 1000000000)))
-	}
-	go func() {
-		i := 0
-		for {
-			<-t
-			out <- i
-			fmt.Printf("%s: %d\n", name, i)
-			i++
-		}
-	}()
-	return out
-
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
 
+// 1 OMIT
+func race(name string, raceTo int) chan struct{} {
+	done := make(chan struct{})
+
+	go func() {
+
+		// returns a channel will send the current time at a random interval
+		t := time.Tick(time.Duration(int(rand.Float64() * float64(time.Second))))
+		for i := 0; i < raceTo; i++ {
+			<-t
+			fmt.Printf("%s says: %d\n", name, i)
+		}
+
+		// signal that we are done counting
+		done <- struct{}{}
+		close(done)
+	}()
+	return done
+}
+
+// 2 OMIT
+
 func main() {
-	eliot := race("Eliot", true)
-	nolen := race("Nolen", false)
-
 	// START OMIT
-	count := 0
-	for {
-		select {
-		case count = <-eliot:
-			if count == 9 {
-				println("Eliot wins")
-			}
+	eliot := race("Eliot", 10)
+	nolen := race("Nolen", 10)
 
-		case count = <-nolen:
-			if count == 9 {
-				println("Nolen wins")
-			}
-		}
+	select {
+	case <-eliot:
+		println("Eliot wins, Nolen is a dumb dumb.")
 
-		if count == 9 {
-			break
-		}
+	case <-nolen:
+		println("Nolen wins, Eliot is a dumb dumb.")
 	}
 	// END OMIT
-
 }
